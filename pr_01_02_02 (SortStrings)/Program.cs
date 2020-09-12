fdc98505707908bdf025695204c53d08dc705005
Курящий дед <mic.makarov@mail.ru>
2020-09-12 22:10:20 +0300

    Создана практика 01 02 02

---
+++ pr_01_02_02 (SortStrings)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pr_01_02_02__SortStrings_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Эта строка будет разбита с разделителем ' ' и добавлена в массив sa, затем массив будет отсортирован и выведен на экран";
+            Console.WriteLine(s);
+            string[] sa = s.Split(" ");
+            Array.Sort(sa);
+            s = string.Join(" ", sa); 
+            Console.WriteLine(s);
+        }
+    }
+}
